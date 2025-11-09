c9759aad95f9fcf2ab4877470c5f9af23fa9d7df
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-11-09 16:24:29 +0300

    feat: add Error handling & async

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,60 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const value = 'Hello, World!';
+    const result = await resolveValue(value);
+    expect(result).toBe(value);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    expect.assertions(1);
+    try {
+      throwError('Something went wrong!');
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe('Something went wrong!');
+      } else {
+        fail('Expected an instance of Error');
+      }
+    }
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect.assertions(1);
+    try {
+      throwError();
+    } catch (error) {
+      if (error instanceof Error) {
+        expect(error.message).toBe('Oops!');
+      } else {
+        fail('Expected an instance of Error');
+      }
+    }
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect.assertions(1);
+    try {
+      throwCustomError();
+    } catch (error) {
+      expect(error).toBeInstanceOf(MyAwesomeError);
+    }
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
